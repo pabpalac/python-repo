e239d320803dc457576e0a987afbe34e5e0506b4
Pablo Palacios <pabpalac@gmail.com>
2022-09-10 23:26:59 +0200

    Finiquitau

--- coeficientes-binomiales/binomial-exponent.py
+++ coeficientes-binomiales/binomial-exponent.py
@@ -1,32 +1,39 @@
-def get_row(row_index):
-    cur_row = [1]
-    if row_index == 0:
-        return cur_row
-    prev = get_row(row_index - 1)
-    for i in range(1, len(prev)):
-        curr = prev[i - 1] + prev[i]
-        cur_row.append(curr)
-    cur_row.append(1)
-    return cur_row
+def obtener_fila_piramide_pascal(indice_de_fila):
+    esta_fila = [1]
+    if indice_de_fila == 0:
+        return esta_fila
+    anterior = obtener_fila_piramide_pascal(indice_de_fila - 1)
+    for i in range(1, len(anterior)):
+        esta_fila.append(anterior[i - 1] + anterior[i])
+    esta_fila.append(1)
+    return esta_fila
 
 
-def main():
-    exponent = int(input("Introduce the binomial exponent: (a+b)^(?): "))
-    pascal_row = get_row(exponent)
-    pascal_row_length = len(pascal_row)
-    a_exponent = pascal_row_length
-    b_exponent = 1
-    for i in range(pascal_row_length):
-        a_exponent = a_exponent - i
-        b_exponent = b_exponent + i
-        current_item = pascal_row[i]
-        if current_item == 1:
-            if i < exponent / 2:
-                print("a^" + str(exponent), end=" + ")
+def principal():
+    exponente = int(input("Introduce the binomial exponent: (a+b)^(?): "))
+    fila_pascal = obtener_fila_piramide_pascal(exponente)
+    tamanio_fila_pascal = len(fila_pascal)
+    exponente_a = tamanio_fila_pascal - 2
+    exponente_b = 1
+    for i in range(tamanio_fila_pascal):
+        este_valor = fila_pascal[i]
+        if este_valor == 1:
+            if i < exponente / 2:
+                print("a^" + str(exponente), end=" + ")
             else:
-                print("b^" + str(exponent))
+                print("b^" + str(exponente))
         else:
-            print(str(pascal_row[i]) + "a^" + str(a_exponent) + "b^" + str(b_exponent), end=" + ")
+            print(este_valor, end="")
+            if exponente_a == 1:
+                print("a", end="")
+            else:
+                print("a^" + str(exponente_a), end="")
+            if exponente_b == 1:
+                print("b", end=" + ")
+            else:
+                print("b^" + str(exponente_b), end=" + ")
+            exponente_a = exponente_a - 1
+            exponente_b = exponente_b + 1
 
 
-main()
+principal()
